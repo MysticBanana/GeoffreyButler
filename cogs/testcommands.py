9c6db95ed72eb2c3e478d2797ee4fb8bde2ee63c
MysticBanana <89936555+MysticBanana@users.noreply.github.com>
2022-02-19 02:07:06 +0100

    init commit of basic structure

---
+++ cogs/testcommands.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class GeoffreyCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_check(self, ctx):
+        pass
+
+    @commands.command(name="test", description="testing")
+    async def test(self, ctx):
+        pass
+
+def setup(bot):
+    bot.add_cog(GeoffreyCommands(bot))
---
+++ core/bot.py
@@ -0,0 +1,43 @@
+import discord
+import os
+import cogs
+from discord.ext import tasks, commands
+from pathlib import Path
+import datetime
+
+
+class Geoffrey(commands.Bot):
+    project_root = Path(__file__).parent.parent
+    VERSION = 1.0
+
+    def __init__(self, command_prefix="?", **options):
+        super().__init__(command_prefix, **options)
+
+        cogs.testcommands.setup(self)
+
+        with open(os.path.join(self.project_root, "TOKEN.txt"), "r") as token_file:
+            self.token = token_file.read().strip()
+
+    async def on_ready(self):
+        await self.change_presence(activity=discord.Game(name=f'{self.command_prefix}help || Version: {self.VERSION}'))
+        if not hasattr(self, 'uptime'):
+            self.uptime = datetime.datetime.utcnow()
+
+    async def on_command_error(self, context, exception):
+        if context.message:
+            await context.message.delete()
+
+        # Cooldown on command triggered
+        if type(exception) == commands.CommandOnCooldown:
+            pass
+
+        if type(exception) == commands.CommandInvokeError:
+            print("i messed up sowwy")
+
+    def run(self):
+        super().run(self.token)
+
+
+if __name__ == "__main__":
+    bot = Geoffrey()
+    bot.run()
